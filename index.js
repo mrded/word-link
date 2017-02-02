ce6c665aff0916c0450181ef5bc124041b0daf6d
Dmitry Demenchuk <dmitry@demenchuk.me>
2017-02-02 17:37:40 +0000

    Release of implementation

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 'use strict';
+
+var _uniq = function(items) {
+  return items.filter(function(item, pos) {
+    return items.indexOf(item) == pos;
+  });
+};
+
+var _createLink = function(text, url) {
+  return '<a href=":url" target="_blank">:text</a>'.replace(':text', text).replace(':url', url);
+}
+
+var _replaceTextElement = function(element, word, url, opts) {
+  var pattern = "\\b:text\\b";
+
+  var findRegexp = new RegExp(pattern.replace(':text', word), 'ig');
+  var found = element.data.match(findRegexp);
+
+  if (found) {
+    found = _uniq(found);
+
+    for (var j in found) {
+      var newElement = document.createElement(null);
+
+      var foundRegexp = new RegExp(pattern.replace(':text', found[j]), 'g');
+      newElement.innerHTML = element.data.replace(foundRegexp, _createLink(found[j], url));
+
+      element.parentNode.replaceChild(newElement, element);
+    }
+  }
+};
+
+var _replaceTagElement = function(element, word, url, opts) {
+  var excludedTags = ['HTMLHeadingElement', 'HTMLAnchorElement'];
+
+  for (var i in element.childNodes) {
+    var node = element.childNodes[i];
+    var type = node.constructor.name;
+
+    if (type === "Text") {
+      _replaceTextElement(node, word, url, opts);
+    }
+
+    else if (excludedTags.indexOf(type) === -1) {
+      _replaceTagElement(node, word, url, opts);
+    }
+  }
+};
+
+module.exports.applyText = function(text, word, url, opts) {
+  var element = document.createElement(null);
+  element.innerHTML = text;
+
+  applyElement(element, word, url, opts);
+
+  return element.innerHTML;
+};
+
+module.exports.applyElement = function(element, word, url, opts) {
+  _replaceTagElement(element, word, url, opts);
+};
