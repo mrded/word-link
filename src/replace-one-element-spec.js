cf5c76e69e8647013634de01b32206f8fd421670
Dmitry Demenchuk <dmitry@demenchuk.me>
2018-07-02 23:59:08 +0100

    Update tests.

--- src/replace-one-element-spec.js
+++ src/replace-one-element-spec.js
@@ -7,45 +7,39 @@ const dom = new JSDOM('<!doctype html><html><body></body></html>');
 
 global.document = dom.window.document;
 
-const createElement = function(text) {
-  const element = document.createElement('span');
-
-  element.data = text;
-
-  return element;
-};
-
 describe('replace-one-element', function() {
   it('Replace one word', function() {
-    const element = createElement("foo bar baz");
+    const element = document.createTextNode("foo bar baz");
     const newElement = ReplaceOneElement(element, 'bar', '#', {});
 
     Expect(newElement.innerHTML).to.equal('foo <a href="#">bar</a> baz');
   });
 
   it('Replace two words', function() {
-    const element = createElement("foo bar foo");
+    const element = document.createTextNode("foo bar foo");
     const newElement = ReplaceOneElement(element, 'foo', '#', {});
 
     Expect(newElement.innerHTML).to.equal('<a href="#">foo</a> bar <a href="#">foo</a>');
   });
 
   it('Replace only whole words', function() {
-    const element = createElement("foobar baz");
+    const element = document.createTextNode("foobar baz");
     const newElement = ReplaceOneElement(element, 'foo', '#', {});
 
-    Expect(newElement.innerHTML).to.equal('foobar baz');
+    // Because text elements don't have innerHTML, only textContent.
+    Expect(newElement.innerHTML).to.equal(undefined);
+    Expect(newElement.textContent).to.equal('foobar baz');
   });
 
   it('Replace one word with regex', function() {
-    const element = createElement("foo bar baz");
+    const element = document.createTextNode("foo bar baz");
     const newElement = ReplaceOneElement(element, '(bar)', '#', {});
 
     Expect(newElement.innerHTML).to.equal('foo <a href="#">bar</a> baz');
   });
 
   it('Replace two words with regex', function() {
-    const element = createElement("foo bar baz");
+    const element = document.createTextNode("foo bar baz");
     const newElement = ReplaceOneElement(element, '(foo|baz)', '#', {});
 
     Expect(newElement.innerHTML).to.equal('<a href="#">foo</a> bar <a href="#">baz</a>');
