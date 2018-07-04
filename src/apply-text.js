a707af37adc85bbd2e277593514cf6318820a2c6
Dmitry Demenchuk <dmitry@demenchuk.me>
2018-07-04 15:31:33 +0100

    Remove a hack.

--- src/apply-text.js
+++ src/apply-text.js
@@ -6,10 +6,6 @@ module.exports = function(text, word, url, opts) {
 
   const newElement = applyElement(element, word, url, opts);
 
-  // The processed text will be wrapped into <SPAN>. 
-  // Return firstChild to keep it plaintext.
-  return newElement.firstChild.nodeName === '#text'
-    ? newElement.innerHTML
-    : newElement.firstChild.innerHTML;
+  return newElement.innerHTML;
 };
 
